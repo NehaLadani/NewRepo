4fde91df0ec9d6c3c2e0b5adc31f338f31b8079b
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-07 17:33:17 -0400

    Made a change for the line to be printed

--- NewRepo/Program.cs
+++ NewRepo/Program.cs
@@ -7,6 +7,7 @@ namespace NewRepo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine($"How r u all");
         }
     }
 }
